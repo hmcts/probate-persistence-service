6a4b453076205a8daafd17d331c8847d9e4df853
praveenr <praveen.moj@gmail.com>
2018-04-24 13:35:08 +0100

    fixed cnp issue

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -35,7 +35,6 @@ module "probate-persistence-service" {
   ilbIp = "${var.ilbIp}"
   is_frontend  = false
   subscription = "${var.subscription}"
-  additional_host_name = "${var.external_host_name}"
 
   app_settings = {
 
