7fb15fa66307d15830503e1de293314b598bfca2
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2018-04-26 14:00:23 +0100

    PRO-2409: add endpoint for sequence numbers

---
+++ src/test/java/uk/gov/hmcts/probate/services/persistence/controllers/SequenceNumberControllerTest.java
@@ -0,0 +1,45 @@
+package uk.gov.hmcts.probate.services.persistence.controllers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.jdbc.support.incrementer.PostgreSQLSequenceMaxValueIncrementer;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Map;
+
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest()
+public class SequenceNumberControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    Map<String, PostgreSQLSequenceMaxValueIncrementer> registrySequenceNumbers;
+
+    @Mock
+    PostgreSQLSequenceMaxValueIncrementer mockIncrementer;
+
+    @Test
+    public void getNext() throws Exception {
+        when(this.registrySequenceNumbers.get(anyString()))
+                .thenReturn(mockIncrementer);
+        when(this.mockIncrementer.nextLongValue())
+                .thenReturn(1234L);
+
+        this.mvc.perform(get("/seq/oxford"))
+                .andExpect(status().isOk()).andExpect(content().json("1234"));
+
+    }
+}
