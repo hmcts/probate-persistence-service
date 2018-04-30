cdceeb12cf6faed1dd05a51a901552b1741cf3b4
Nigel Dunne <12184413+nigeldunne@users.noreply.github.com>
2018-04-30 18:26:27 +0100

    PRO-2409: add exception for unconfigured registry

---
+++ src/main/java/uk/gov/hmcts/probate/services/persistence/services/SequenceNumberService.java
@@ -0,0 +1,25 @@
+package uk.gov.hmcts.probate.services.persistence.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.config.ResourceNotFoundException;
+import org.springframework.jdbc.support.incrementer.PostgreSQLSequenceMaxValueIncrementer;
+import org.springframework.stereotype.Service;
+import uk.gov.hmcts.probate.services.persistence.model.RegistryNotConfiguredException;
+
+import java.util.Map;
+
+@Service
+public class SequenceNumberService {
+
+    @Autowired
+    private Map<String, PostgreSQLSequenceMaxValueIncrementer> registrySequenceNumbers;
+
+    public Long getNext(String registryName) {
+
+        try {
+            return registrySequenceNumbers.get(registryName).nextLongValue();
+        } catch (NullPointerException e) {
+                throw new RegistryNotConfiguredException("Registry not configured: " + registryName, e);
+        }
+    }
+}
