3029f825fcd44e6edf7f7c35da1d156e26fd85bf
Dave Jones <dave.jones@hmcts.net>
2018-05-23 18:41:43 +0100

    PRO-2654: Updated to use same JSON format at SOLS/Business/Submit applications.

--- src/main/java/uk/gov/hmcts/probate/services/persistence/services/ProbatePersistenceInfoContributor.java
+++ src/main/java/uk/gov/hmcts/probate/services/persistence/services/ProbatePersistenceInfoContributor.java
@@ -1,10 +1,8 @@
 package uk.gov.hmcts.probate.services.persistence.services;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
-import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.info.Info;
@@ -16,36 +14,26 @@ public class ProbatePersistenceInfoContributor implements InfoContributor {
 
     @Value("${git.commit.id}")
     private String commitId;
-    
-    @Value("${git.commit.message.full}")
-    private String commitMessage;
    
     @Value("${git.commit.time}")
     private String commitTime;
     
-    private static final String INFO_GROUP_NAME = "git";
-    private static final String GIT_COMMIT_ID_KEY = "commitId";
-    private static final String GIT_COMMIT_MESSAGE_KEY = "commitMessage";
-    private static final String GIT_COMMIT_TIME_KEY = "commitTime";
-    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z");
+    private static final String INFO_GROUP_NAME_GIT = "git";
+    private static final String INFO_GROUP_NAME_COMMIT = "commit";
+    private static final String GIT_COMMIT_ID_KEY = "id";
+    private static final String GIT_COMMIT_TIME_KEY = "time";
     
 	@Override
 	public void contribute(Info.Builder builder) {
 		
-		String commitDateString;
-		HashMap<String, String> results = new HashMap<String, String>();	
-		
-		try {
-			dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
-			commitDateString = dateFormat.format(new Date(Long.parseLong(commitTime) * 1000 ));
-		} catch ( NumberFormatException nfe ) {
-			commitDateString = null;
-		}
-	
-    	results.put(GIT_COMMIT_ID_KEY, Objects.toString(commitId, ""));
-    	results.put(GIT_COMMIT_MESSAGE_KEY, Objects.toString(commitMessage, ""));	
-    	results.put(GIT_COMMIT_TIME_KEY, Objects.toString(commitDateString, ""));     		
-    	builder.withDetail(INFO_GROUP_NAME, results);
+		HashMap<String, String> commitMap = new HashMap<String, String>();
+		commitMap.put(GIT_COMMIT_TIME_KEY, Objects.toString(commitTime, ""));
+		commitMap.put(GIT_COMMIT_ID_KEY, Objects.toString(commitId, ""));
+		  
+    	HashMap<String, Map<String, String>> gitMap = new HashMap<String, Map<String, String>>();	
+    	gitMap.put(INFO_GROUP_NAME_COMMIT, commitMap );  
+    	
+    	builder.withDetail(INFO_GROUP_NAME_GIT, gitMap );
     	
 	}
 
--- src/test/java/uk/gov/hmcts/probate/services/persistence/services/ProbatePersistenceInfoContributorTest.java
+++ src/test/java/uk/gov/hmcts/probate/services/persistence/services/ProbatePersistenceInfoContributorTest.java
@@ -10,10 +10,8 @@ import org.springframework.test.util.ReflectionTestUtils;
 
 public class ProbatePersistenceInfoContributorTest {
 
-	private final static String GIT_COMMIT_TIME = "1526912708";
     private final static String VALID_COMMIT_ID = "2921eb6292eab0bd9ce8a42b44cf32fe9f4b0069";
-    private final static String VALID_COMMIT_MESSAGE = "PRO-1234: Initial build of code."; 
-    private final static String VALID_COMMIT_TIME = "Mon, 21 May 2018 14:25:08 UTC";
+    private final static String VALID_COMMIT_TIME = "2018-05-23T13:59+0000";
     
     private final static String EMPTY_VALUE = "";
     private ProbatePersistenceInfoContributor infoContributor = new ProbatePersistenceInfoContributor();
@@ -23,16 +21,16 @@ public class ProbatePersistenceInfoContributorTest {
 
         try {
             ReflectionTestUtils.setField(infoContributor, "commitId", VALID_COMMIT_ID);
-            ReflectionTestUtils.setField(infoContributor, "commitMessage", VALID_COMMIT_MESSAGE);
-            ReflectionTestUtils.setField(infoContributor, "commitTime", GIT_COMMIT_TIME);
+            ReflectionTestUtils.setField(infoContributor, "commitTime", VALID_COMMIT_TIME);
             
             Info.Builder builder = new Info.Builder();
             infoContributor.contribute(builder);
 
-            Map<String, String> gitMap = (Map<String, String>) builder.build().getDetails().get("git");
-            assertEquals("Checking valid git commit id", VALID_COMMIT_ID, gitMap.get("commitId"));            
-            assertEquals("Checking valid git commit message", VALID_COMMIT_MESSAGE, gitMap.get("commitMessage"));
-            assertEquals("Checking valid git commit date", VALID_COMMIT_TIME, gitMap.get("commitTime"));
+            @SuppressWarnings("unchecked")
+			Map<String, Map<String, String>> gitMap = (Map<String, Map<String, String>>) builder.build().getDetails().get("git");
+            Map<String, String> commitMap = (Map<String, String>) gitMap.get("commit");
+            assertEquals("Checking valid git commit id", VALID_COMMIT_ID, commitMap.get("id"));            
+            assertEquals("Checking valid git commit date", VALID_COMMIT_TIME, commitMap.get("time"));
 
         } catch (Exception e){
             assert(false);
@@ -46,13 +44,13 @@ public class ProbatePersistenceInfoContributorTest {
             Info.Builder builder = new Info.Builder();
             infoContributor.contribute(builder);
             
-            Map<String, String> gitMap = (Map<String, String>) builder.build().getDetails().get("git");
-            assertEquals("Checking empty git commit id", EMPTY_VALUE, gitMap.get("commitId"));            
-            assertEquals("Checking empty git commit message", EMPTY_VALUE, gitMap.get("commitMessage"));
-            assertEquals("Checking empty git commit date", EMPTY_VALUE, gitMap.get("commitTime"));            
+            @SuppressWarnings("unchecked")
+			Map<String, Map<String, String>> gitMap = (Map<String, Map<String, String>>) builder.build().getDetails().get("git");
+            Map<String, String> commitMap = (Map<String, String>) gitMap.get("commit");
+            assertEquals("Checking empty git commit id", EMPTY_VALUE, commitMap.get("id"));            
+            assertEquals("Checking empty git commit date", EMPTY_VALUE, commitMap.get("time"));            
 
         } catch (Exception e){
-        	e.printStackTrace();
             assert(false);
         }
     }  
@@ -62,16 +60,16 @@ public class ProbatePersistenceInfoContributorTest {
     	
         try {
             ReflectionTestUtils.setField(infoContributor, "commitId", EMPTY_VALUE);
-            ReflectionTestUtils.setField(infoContributor, "commitMessage", VALID_COMMIT_MESSAGE);
-            ReflectionTestUtils.setField(infoContributor, "commitTime", GIT_COMMIT_TIME);
+            ReflectionTestUtils.setField(infoContributor, "commitTime", VALID_COMMIT_TIME);
             
             Info.Builder builder = new Info.Builder();
             infoContributor.contribute(builder);
             
-            Map<String, String> gitMap = (Map<String, String>) builder.build().getDetails().get("git");
-            assertEquals("Checking empty git commit id", EMPTY_VALUE, gitMap.get("commitId"));   
-            assertEquals("Checking valid git commit message", VALID_COMMIT_MESSAGE, gitMap.get("commitMessage"));
-            assertEquals("Checking valid git commit date", VALID_COMMIT_TIME, gitMap.get("commitTime"));            
+            @SuppressWarnings("unchecked")
+			Map<String, Map<String, String>> gitMap = (Map<String, Map<String, String>>) builder.build().getDetails().get("git");
+            Map<String, String> commitMap = (Map<String, String>) gitMap.get("commit");
+            assertEquals("Checking empty git commit id", EMPTY_VALUE, commitMap.get("id"));   
+            assertEquals("Checking valid git commit date", VALID_COMMIT_TIME, commitMap.get("time"));            
 
         } catch (Exception e){
             assert(false);
