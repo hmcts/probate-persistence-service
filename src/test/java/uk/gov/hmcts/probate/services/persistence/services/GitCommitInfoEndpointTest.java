41133b98c841e949dc29726d85ecabff8cc9b188
Dave Jones <dave.jones@hmcts.net>
2018-05-25 14:59:30 +0100

    PRO-2654: Updated change to fix test failing in Jenkins.

--- src/test/java/uk/gov/hmcts/probate/services/persistence/services/GitCommitInfoEndpointTest.java
+++ src/test/java/uk/gov/hmcts/probate/services/persistence/services/GitCommitInfoEndpointTest.java
@@ -9,6 +9,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.endpoint.MetricsEndpoint;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -23,7 +24,7 @@ import net.minidev.json.parser.JSONParser;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@DirtiesContext
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @TestPropertySource(properties = {"spring.info.git.location=classpath:uk/gov/hmcts/probate/services/persistence/git-test.properties"})
 public class GitCommitInfoEndpointTest {
 
