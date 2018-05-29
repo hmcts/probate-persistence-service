2b0badf0cc324eb988323fc6aea2efec54f40090
Tom Elliott <tomxelliott@hotmail.co.uk>
2018-05-29 14:40:54 +0100

    PRO-2656: Add API endpoint for searching by Submission Ref that was removed

--- src/main/java/uk/gov/hmcts/probate/services/persistence/repository/SubmissionRepository.java
+++ src/main/java/uk/gov/hmcts/probate/services/persistence/repository/SubmissionRepository.java
@@ -4,10 +4,13 @@ package uk.gov.hmcts.probate.services.persistence.repository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import uk.gov.hmcts.probate.services.persistence.model.Formdata;
 import uk.gov.hmcts.probate.services.persistence.model.Submission;
 
 @RepositoryRestResource(collectionResourceRel = "submissions", path = "submissions")
 public interface SubmissionRepository extends PagingAndSortingRepository<Submission, Long> {
 
     Submission findById(@Param("id") Long id);
+
+    Formdata findBySubmissionReference(@Param("submissionReference") long submissionReference);
 }
