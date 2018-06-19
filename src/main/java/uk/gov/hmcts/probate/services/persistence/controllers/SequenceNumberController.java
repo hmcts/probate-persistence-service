11770ef3efade781d960e5781a406f81f4989ee9
Dhruv Patel <dhruv.patel20@gmail.com>
2018-06-19 10:42:19 +0100

    PRO-3060: Added logging.

--- src/main/java/uk/gov/hmcts/probate/services/persistence/controllers/SequenceNumberController.java
+++ src/main/java/uk/gov/hmcts/probate/services/persistence/controllers/SequenceNumberController.java
@@ -1,5 +1,7 @@
 package uk.gov.hmcts.probate.services.persistence.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,13 +13,18 @@ import uk.gov.hmcts.probate.services.persistence.services.SequenceNumberService;
 @RestController
 public class SequenceNumberController {
 
-    @Autowired
-    private SequenceNumberService sequenceNumberService;
+
+  private static final Logger logger = LoggerFactory.getLogger(SequenceNumberController.class);
+
+  @Autowired
+  private SequenceNumberService sequenceNumberService;
 
 
-    @GetMapping(value = "/sequence-number/{registry}")
-    public ResponseEntity<Long> getNext(@PathVariable String registry) {
+  @GetMapping(value = "/sequence-number/{registry}")
+  public ResponseEntity<Long> getNext(@PathVariable String registry) {
 
-        return new ResponseEntity<>(sequenceNumberService.getNext(registry), HttpStatus.OK);
-    }
+    Long sequenceNumber = sequenceNumberService.getNext(registry);
+    logger.info("Request with sequence number: {}, and registry: {} processed", sequenceNumber, registry);
+    return new ResponseEntity<>(sequenceNumber, HttpStatus.OK);
+  }
 }
