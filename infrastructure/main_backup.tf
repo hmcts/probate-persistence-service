52b11ed30746e5ea9b62379303bac922a0286b92
praveenr <praveen.moj@gmail.com>
2018-06-25 19:21:10 +0100

    Testing previrw

--- infrastructure/main_backup.tf
+++ infrastructure/main_backup.tf
@@ -36,20 +36,15 @@ data "vault_generic_secret" "probate_postgresql_port" {
 locals {
   aseName = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
   //java_proxy_variables: "-Dhttp.proxyHost=${var.proxy_host} -Dhttp.proxyPort=${var.proxy_port} -Dhttps.proxyHost=${var.proxy_host} -Dhttps.proxyPort=${var.proxy_port}"
-  app_full_name = "${var.product}-${var.microservice}"
+  app_full_name = "pro-${var.microservice}"
 
-//   //probate_frontend_hostname = "probate-frontend-aat.service.core-compute-aat.internal"
-//   previewVaultName = "pro-persist-ser"
-//   nonPreviewVaultName = "pro-persist-ser-${var.env}"
-//   vaultName = "${(var.env == "preview" || var.env == "spreview") ? local.previewVaultName : local.nonPreviewVaultName}"
-
-//   nonPreviewVaultUri = "${module.probate-persistence-service-vault.key_vault_uri}"
-//   previewVaultUri = "https://pro-persist-ser-aat.vault.azure.net/"
-//   vaultUri = "${(var.env == "preview" || var.env == "spreview") ? local.previewVaultUri : local.nonPreviewVaultUri}"
-
+  //probate_frontend_hostname = "probate-frontend-aat.service.core-compute-aat.internal"
+  // previewVaultName = "pro-pers-ser"
+  // nonPreviewVaultName = "pro-pers-ser-${var.env}"
+  // vaultName = "${(var.env == "preview" || var.env == "spreview") ? local.previewVaultName : local.nonPreviewVaultName}"
 }
 
-module "probate-persistence-service" {
+module "pro-persistence-service" {
   source = "git@github.com:hmcts/moj-module-webapp.git?ref=master"
   product = "${local.app_full_name}"
   location = "${var.location}"
@@ -85,14 +80,15 @@ module "probate-persistence-service" {
   }
 }
 
-module "probate-persistence-service-vault" {
+
+module "pro-persistence-service-vault" {
   source              = "git@github.com:hmcts/moj-module-key-vault?ref=master"
   name                = "pro-pers-ser-${var.env}"
   product             = "${var.product}"
   env                 = "${var.env}"
   tenant_id           = "${var.tenant_id}"
   object_id           = "${var.jenkins_AAD_objectId}"
-  resource_group_name = "${module.probate-persistence-service.resource_group_name}"
+  resource_group_name = "${module.pro-persistence-service.resource_group_name}"
   product_group_object_id = "33ed3c5a-bd38-4083-84e3-2ba17841e31e"
 }
 
@@ -115,29 +111,30 @@ module "probate-persistence-db" {
 resource "azurerm_key_vault_secret" "POSTGRES-USER" {
   name = "${local.app_full_name}-POSTGRES-USER"
   value = "${module.probate-persistence-db.user_name}"
-  vault_uri = "${module.probate-persistence-service-vault.key_vault_uri}"
+  vault_uri = "${module.pro-persistence-service-vault.key_vault_uri}"
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
   name = "${local.app_full_name}-POSTGRES-PASS"
   value = "${module.probate-persistence-db.postgresql_password}"
-  vault_uri = "${module.probate-persistence-service-vault.key_vault_uri}"
+  vault_uri = "${module.pro-persistence-service-vault.key_vault_uri}"
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES_HOST" {
   name = "${local.app_full_name}-POSTGRES-HOST"
   value = "${module.probate-persistence-db.host_name}"
-  vault_uri = "${module.probate-persistence-service-vault.key_vault_uri}"
+  vault_uri = "${module.pro-persistence-service-vault.key_vault_uri}"
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES_PORT" {
   name = "${local.app_full_name}-POSTGRES-PORT"
   value = "${module.probate-persistence-db.postgresql_listen_port}"
-  vault_uri = "${module.probate-persistence-service-vault.key_vault_uri}"
+  vault_uri = "${module.pro-persistence-service-vault.key_vault_uri}"
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES_DATABASE" {
   name = "${local.app_full_name}-POSTGRES-DATABASE"
   value = "${module.probate-persistence-db.postgresql_database}"
-  vault_uri = "${module.probate-persistence-service-vault.key_vault_uri}"
+  vault_uri = "${module.pro-persistence-service-vault.key_vault_uri}"
 }
+
