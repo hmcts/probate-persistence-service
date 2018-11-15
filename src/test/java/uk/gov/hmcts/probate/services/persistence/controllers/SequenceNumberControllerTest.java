da5da4ab23102b9dd089ef42e24ae0d8f027d5e7
Jake <jake.huggins@solirius.com>
2018-11-15 12:00:59 +0000

    PRO-4025: update deprecated import

--- src/test/java/uk/gov/hmcts/probate/services/persistence/controllers/SequenceNumberControllerTest.java
+++ src/test/java/uk/gov/hmcts/probate/services/persistence/controllers/SequenceNumberControllerTest.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import uk.gov.hmcts.probate.services.persistence.services.SequenceNumberService;
 
-import static org.mockito.Matchers.anyString;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
