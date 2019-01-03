8d5154bcb072bfd61e79a6d666d73743ea97550d
Suneet Kamath <suneet.kamath@hmcts.net>
2019-01-03 12:13:36 +0000

    PRO-4025 fix for Sonar issue

--- src/main/java/uk/gov/hmcts/probate/services/persistence/controllers/FormdataController.java
+++ src/main/java/uk/gov/hmcts/probate/services/persistence/controllers/FormdataController.java
@@ -5,8 +5,8 @@ import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.hateoas.ExposesResourceFor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import uk.gov.hmcts.probate.services.persistence.model.Formdata;
@@ -21,9 +21,8 @@ public class FormdataController {
     @Autowired
     FormdataRepository formdataRepository;
 
-    @RequestMapping(method = RequestMethod.GET, value = "/search/findById")
-    public @ResponseBody
-    ResponseEntity<Formdata> findById(@RequestParam("id") String id) {
+    @GetMapping(value = "/search/findById")
+    public @ResponseBody ResponseEntity<Formdata> findById(@RequestParam("id") String id) {
         Optional<Formdata> byId = formdataRepository.findById(id);
         return byId
                 .map(formData -> new ResponseEntity<>(formData, HttpStatus.FOUND))
--- src/main/java/uk/gov/hmcts/probate/services/persistence/controllers/InvitedataController.java
+++ src/main/java/uk/gov/hmcts/probate/services/persistence/controllers/InvitedataController.java
@@ -5,8 +5,8 @@ import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.hateoas.ExposesResourceFor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import uk.gov.hmcts.probate.services.persistence.model.Invitedata;
@@ -21,7 +21,7 @@ public class InvitedataController {
     @Autowired
     InvitedataRepository invitedataRepository;
 
-    @RequestMapping(method = RequestMethod.GET, value = "/search/findById")
+    @GetMapping(value = "/search/findById")
     public @ResponseBody ResponseEntity<Invitedata> findById(@RequestParam("id") String id) {
         Optional<Invitedata> byId = invitedataRepository.findById(id);
         return byId
