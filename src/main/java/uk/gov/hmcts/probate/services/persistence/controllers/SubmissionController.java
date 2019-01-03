bc778e96c4fda19583b5a1d66e5d2b66ae65fc03
Suneet Kamath <suneet.kamath@hmcts.net>
2019-01-03 12:13:36 +0000

    PRO-4025 fix for Sonar issue

--- src/main/java/uk/gov/hmcts/probate/services/persistence/controllers/SubmissionController.java
+++ src/main/java/uk/gov/hmcts/probate/services/persistence/controllers/SubmissionController.java
@@ -5,8 +5,8 @@ import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.hateoas.ExposesResourceFor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import uk.gov.hmcts.probate.services.persistence.model.Submission;
@@ -21,7 +21,7 @@ public class SubmissionController {
     @Autowired
     SubmissionRepository submissionRepository;
 
-    @RequestMapping(method = RequestMethod.GET, value = "/search/findById")
+    @GetMapping(value = "/search/findById")
     public @ResponseBody ResponseEntity<Submission> findById(@RequestParam("id") Long id) {
         Optional<Submission> byId = submissionRepository.findById(id);
         return byId
