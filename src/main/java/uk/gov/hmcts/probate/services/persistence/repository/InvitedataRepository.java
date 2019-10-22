3d22cb5675b45a323d9371f080075dd74c4232a6
ruban72 <ruban72@hotmail.com>
2019-10-22 11:49:17 +0100

    PRO-6212: add query by create date for invite data

--- src/main/java/uk/gov/hmcts/probate/services/persistence/repository/InvitedataRepository.java
+++ src/main/java/uk/gov/hmcts/probate/services/persistence/repository/InvitedataRepository.java
@@ -1,12 +1,18 @@
 package uk.gov.hmcts.probate.services.persistence.repository;
 
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.format.annotation.DateTimeFormat;
+import uk.gov.hmcts.probate.services.persistence.model.Formdata;
 import uk.gov.hmcts.probate.services.persistence.model.Invitedata;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,4 +23,7 @@ public interface InvitedataRepository extends PagingAndSortingRepository<Invited
 
     @RestResource(path = "formdata")
     List<Invitedata> findByFormdataId(@Param("id") String id);
+
+    @Query("select i from invitedata i where  i.creationTime >= :startDate")
+    Page<Invitedata> findByCreatedAfterDate(@Param("startDate") @DateTimeFormat(pattern = "dd/MM/yyyy") Date startDate, Pageable page);
 }
